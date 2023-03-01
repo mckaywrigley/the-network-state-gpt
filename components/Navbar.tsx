0749ab773b2adb0e610935a6dae6b3a31b9dfe88
Mckay Wrigley <mckaywrigley@gmail.com>
2023-03-01 09:18:16 -0700

    hide img on mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,6 +8,7 @@ export const Navbar: FC = () => {
     <div className="flex h-[60px] border-b border-gray-300 py-2 px-8 items-center justify-between">
       <div className="font-bold text-2xl flex items-center">
         <Image
+          className="hidden sm:flex"
           src={cover}
           alt="The Network State GPT"
           width={30}
